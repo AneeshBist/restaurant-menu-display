f38c710d2a4f284f1dd3f7443e47a274785b50da
AneeshBist <aneeshbist@gmail.com>
2023-06-25 19:50:24 +0530

    created a restaurant menu view using react js

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,25 @@ import React from "react";
 
 const Card = ({ item }) => {
   return (
-    <div className="card p-2">
-      <div className="d-flex align-item-center ">
-        <div className="">
-          <span className="image">
-            <img src={item.img} alt="" style={{ width: 50 }} />
-          </span>
-        </div>
-        <div className="d-flex flex-column">
-          <span className="name"> item: {item.name}</span>
-          <span className="price"> price: {item.price}</span>
-          <span className="description">details: {item.description}</span>
+    <div className="col-md-4 mt-5">
+      <div className="">
+        <div className="card p-2">
+          <div className="d-flex align-item-center ">
+            <div className="image align-self-center">
+              <img
+                src={item.img}
+                alt={item.name}
+                className="rounded"
+                style={{ width: "5rem" }}
+              />
+            </div>
+
+            <div className="d-flex flex-column align-items-end p-2">
+              <span className="name">{item.name}</span>
+              <span className="price">{item.price}</span>
+              <span className="description">**{item.description}** </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,20 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
+import Menu from "../data/menu";
 
 const Dashboard = () => {
-  const item = {
-    id: 5,
-    img: "./img/aloo-masala.jpg",
-    name: "aloo masala",
-    category: "dinner",
-    price: "₹ 115/-",
-    description: "Loerm Ipsum Loerm Ipsum Loerm Ipsum Loerm Ipsum Loerm Ipsum ",
+  const [selector, setSelector] = useState("all");
+  const menuItems = Menu;
+
+  const filterMenu = (e) => {
+    e.preventDefault();
+    setSelector(e.target.value);
   };
 
   return (
     <div className="container-fluid">
-      Welcome to the dashbord
-      <Card item={item} />
+      <div className="row text-center justify-content-center mb-5">
+        <div>
+          <h1 className="mt-2 pg-title">Yumm Yumm Menu</h1>
+          <div className="col-6 offset-3 ">
+            <div className="filters d-flex justify-content-around mt-3">
+              <button
+                className={`btn ${
+                  selector === "all" ? "btn-select" : "btn-warning"
+                }`}
+                value="all"
+                onClick={filterMenu}
+              >
+                All
+              </button>
+              <button
+                className={`btn ${
+                  selector === "breakfast" ? "btn-select" : "btn-warning"
+                }`}
+                value="breakfast"
+                onClick={filterMenu}
+              >
+                Breakfast
+              </button>
+              <button
+                className={`btn ${
+                  selector === "lunch" ? "btn-select" : "btn-warning"
+                }`}
+                value="lunch"
+                onClick={filterMenu}
+              >
+                Lunch
+              </button>
+              <button
+                className={`btn ${
+                  selector === "evening" ? "btn-select" : "btn-warning"
+                }`}
+                value="evening"
+                onClick={filterMenu}
+              >
+                Evening
+              </button>
+              <button
+                className={`btn ${
+                  selector === "dinner" ? "btn-select" : "btn-warning"
+                }`}
+                value="dinner"
+                onClick={filterMenu}
+              >
+                Dinner
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {menuItems
+          .filter((item) =>
+            selector === "all" ? item : item.category === selector
+          )
+          .map((item) => {
+            return <Card item={item} key={item.id} />;
+          })}
+      </div>
     </div>
   );
 };
